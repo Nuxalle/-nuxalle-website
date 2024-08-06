ca903f99eb0db35a1efd9e725babcc09313fa883
mac <emmanuelkelechi@verydeeptech.com>
2024-08-06 12:49:49 +0100

    code spin up with tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
           priority
         />
       </div>
-      <Button>Nuxalle Project Spin-up</Button>
+      <Button> Nuxalle Project Spin-up</Button>
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
